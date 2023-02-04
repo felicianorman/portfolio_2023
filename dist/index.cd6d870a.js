be6bf510e167d434be7c692b93accd990ebf0122
Felicia <87656480+felicianorman@users.noreply.github.com>
2023-02-04 22:03:29 +0100

    Add files via upload

---
+++ dist/index.cd6d870a.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector(".hamburger");
+var navMenu = document.querySelector(".navbar--menu");
+hamburger === null || hamburger === void 0 || hamburger.addEventListener("click", function() {
+    console.log("Hej");
+    hamburger.classList.add("active");
+    navMenu === null || navMenu === void 0 || navMenu.classList.add("active");
+});
+
+//# sourceMappingURL=index.cd6d870a.js.map
